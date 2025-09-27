f5a73b4b16424bc30257782ab7031a38d91a0e2c
Gaurav Patrange <gauravpatrange286@gmail.com>
2025-09-27 12:49:51 +0530

    Initial commit

---
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,38 @@
+package com.eazybytes.accounts;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title="Accounts microservices REST API Documentation",
+				description = "EazyBack Accounts microservice REST API Documentation",
+				version="v1",
+				contact = @Contact(
+						name = "Gaurav Patrange",
+						email = "gaurav@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "demo.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Accounts microservices REST Api Documentation"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
