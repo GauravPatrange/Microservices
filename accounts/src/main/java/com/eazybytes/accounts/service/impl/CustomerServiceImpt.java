5559352034c83c558692e3dad2fc61678431d155
Gaurav Patrange <gauravpatrange286@gmail.com>
2025-11-15 14:41:46 +0530

    Implement Resilient in microserivces

--- accounts/src/main/java/com/eazybytes/accounts/service/impl/CustomerServiceImpt.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/impl/CustomerServiceImpt.java
@@ -43,10 +43,16 @@ public class CustomerServiceImpt implements ICustomersService {
         customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
 
         ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
-        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        if(loansDtoResponseEntity != null){
+            customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        }
+
 
         ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId, mobileNumber);
-        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        if(cardsDtoResponseEntity != null){
+            customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        }
+
 
         return customerDetailsDto;
     }
